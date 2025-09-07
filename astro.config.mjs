468d5a252136fb8349854ca95ba42d916b4e87f4
Alex Machorro <amachorro02@gmail.com>
2025-09-07 16:24:42 -0700

    upgrade + apply tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [],
-  site: 'https://kiplings.lol/'
+  site: 'https://kiplings.lol/',
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
